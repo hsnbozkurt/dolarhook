ad63f8f88cc0869629163a49351173752966a713
SadMap <71943073+SadMap@users.noreply.github.com>
2021-11-19 19:11:43 +0700

    Fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch")
 const Discord = require("discord.js")
 //const hook2 = new Discord.WebhookClient({id:"webhookid",token:"token"})
-const hook = new Discord.WebhookClientt({id:"webhookid",token:"token"})
+const hook = new Discord.WebhookClient({id:"webhookid",token:"token"})
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 let dolar = 0
 setInterval(function () {
